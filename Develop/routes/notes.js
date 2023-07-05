421f4b999e4239e2a263971e3df3eb3a592db64d
Roxy Osorio <roxy.osorio@moo.com>
2023-07-05 11:46:42 -0400

    cleaned up code

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
-// var uniqid = require('uniqid');
 const fs = require('fs')
-
+let dbJSON = require("../db/db.json")
 
 router.get('/api/notes', async (req, res) => {
-    const dbJSON = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    dbJSON = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
     res.json(dbJSON);
   });
   
   // Defines the post request to this routes end point '/api/notes'
 router.post('/api/notes', (req, res) => {
-    const dbJSON = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    dbJSON = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    console.log("POST",req.body)
     const newNote = {
       title: req.body.title,
-      text: req.body.text
+      text: req.body.text,
+      id: Math.floor(Math.random()*999)
     };
     dbJSON.push(newNote);
     fs.writeFileSync("db/db.json",JSON.stringify(dbJSON));
