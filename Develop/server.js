f2135b6741dc4e5f64e251cc71166aeb3a1abfb8
Roxy Osorio <roxy.osorio@moo.com>
2023-07-04 22:22:14 -0400

    routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const fs = require ('fs');
 const path = require('path');
 // const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3005;
 const app = express();
 const db = (require('./db/db.json'));
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -19,9 +20,14 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
